9e1847c3bed1cd6370561a8ba9884fa7b655dea8
Aaron Larner <aaron@larner.com>
2020-06-25 08:55:15 -0400

    Updates

--- src/components/link.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import { getLocalizedPath } from '../lib/i18n';
-export default (props) => {
-    return <Link {...props} to={getLocalizedPath(props.locale, props.to)} />
-};
